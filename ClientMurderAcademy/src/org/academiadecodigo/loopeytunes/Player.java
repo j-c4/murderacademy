7d0157246ef75e71ee1cc89b16f3670c9f89c1d2
Code Cadet <bruno.januario1998@gmail.com>
2021-10-31 11:39:03 +0000

    all organized and jar working

---
+++ ClientMurderAcademy/src/org/academiadecodigo/loopeytunes/Player.java
@@ -0,0 +1,105 @@
+package org.academiadecodigo.loopeytunes;
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Player {
+
+    private Socket playerSocket;
+    private String name;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Prompt terminalPrompt;
+
+    public Player(String serverIP, int serverPort) {
+
+        try {
+
+            playerSocket = new Socket(serverIP, serverPort);
+
+            in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+            out = new PrintWriter(playerSocket.getOutputStream());
+
+            terminalPrompt = new Prompt(System.in, System.out);
+
+        } catch (IOException e) {
+            System.out.println("Unable to establish a connection to server.");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        if (!(args.length==1)) {
+            System.out.println("YOU NEED TO PUT THE SERVER IP ADDRESS AS ARGUMENT WHEN YOU RUN THE JAR FILE\n");
+            return;
+        }
+
+        Player player = new Player(args[0], 9001);
+
+        try {
+            player.playing();
+        } catch (IOException e) {
+            System.out.println("Server was abruptly shut down.");
+            System.exit(0);
+        }
+
+    }
+
+    public void playing() throws IOException {
+
+        StringInputScanner askName = new StringInputScanner();
+        askName.setMessage("What is your name?");
+        name = terminalPrompt.getUserInput(askName);
+
+        out.println(name);
+        out.flush();
+        String message = "";
+
+        while ((message = in.readLine()) != null) {
+
+            System.out.println(message);
+
+            if (message.contains("It's your turn to guess!")) {
+                guess();
+            }
+
+            if (message.contains("GAME IS OVER")) {
+                playerSocket.close();
+                in.close();
+                out.close();
+                System.exit(0);
+            }
+        }
+    }
+
+    public void guess() {
+        String[] culpritOptions = {"SID", "PRIS", "VANDO", "PEDRO", "MIGUEL"};
+        String[] weaponOptions = {"SKATE", "KEYBOARD", "PUFF", "FIRST MASTER CODER EXCALIBUR", "MEGAPHONE"};
+        String[] murderSceneOptions = {"BATHROOM", "GAMINGROOM", "MCROOM", "CLASSROOM", "GRASS"};
+
+        MenuInputScanner culprits = new MenuInputScanner(culpritOptions);
+        MenuInputScanner weapons = new MenuInputScanner(weaponOptions);
+        MenuInputScanner murderScenes = new MenuInputScanner(murderSceneOptions);
+
+        culprits.setMessage("Who's the culprit?");
+        weapons.setMessage("What was the weapon used?");
+        murderScenes.setMessage("Where did it happen?");
+
+        int choice1 = terminalPrompt.getUserInput(culprits);
+        int choice2 = terminalPrompt.getUserInput(weapons);
+        int choice3 = terminalPrompt.getUserInput(murderScenes);
+
+        String playerGuess = "--->" + "Detective " + name + " thinks " + culpritOptions[choice1 - 1] + " killed the code cadet with a " + weaponOptions[choice2 - 1] + " in the " + murderSceneOptions[choice3 - 1];
+        out.println(playerGuess);
+        out.flush();
+
+    }
+}
---
+++ ServerMurderAcademy/src/org/academiadecodigo/loopeytunes/Server.java
@@ -0,0 +1,191 @@
+package org.academiadecodigo.loopeytunes;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket playerSocket;
+    private LinkedList<ClientConnection> clientConnections;
+    private ExecutorService threadPool;
+    private BufferedReader in;
+    private Game game;
+    private int counter = 0;
+
+    private Server() {
+
+        try {
+
+            serverSocket = new ServerSocket(9001);
+            clientConnections = new LinkedList<>();
+            threadPool = Executors.newFixedThreadPool(4);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        awaitingConnections();
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+
+    private void awaitingConnections() {
+
+        for (int i = 0; i < 4; i++) {
+
+            try {
+
+                playerSocket = serverSocket.accept();
+
+                in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+
+                ClientConnection newPlayer = new ClientConnection(playerSocket, in.readLine());
+                clientConnections.add(newPlayer);
+                threadPool.submit(newPlayer);
+                System.out.println(newPlayer.name + " is connected");
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+        sendStory();
+        game = new Game();
+        sendAll("Game is on!\n");
+
+        next();
+    }
+
+    public static Object readFile(URL url) throws IOException, ClassNotFoundException {
+        ObjectInputStream is = new ObjectInputStream(url.openStream());
+        Object o = is.readObject();
+        is.close();
+        return o;
+    }
+
+    private void sendStory() {
+        String[] text = IntroductionText.getText();
+        int delay = 4000;
+
+        for (int i=0; i < text.length; i++) {
+
+            try {
+                if (text[i].contains("/animation")) {
+                    delay = 150;
+                    continue;
+                }
+                if (text[i].contains("/text")) {
+                    delay = 4000;
+                    continue;
+                }
+                Thread.sleep(delay);
+                sendAll(text[i]);
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void sendAll(String message) {
+        for (ClientConnection cc : clientConnections) {
+            cc.send(message);
+        }
+    }
+
+    private void next() {
+        if (counter == clientConnections.size()) {
+            sendAll(game.getHint());
+            counter = 0;
+        }
+        clientConnections.get(counter).send("It's your turn to guess!");
+        counter++;
+    }
+
+    private void win() {
+        sendAll(game.getConfession());
+        sendAll("GAME IS OVER\n");
+
+        for (int i = 0; i < clientConnections.size(); i++) {
+            clientConnections.remove(clientConnections.get(i));
+        }
+
+        System.out.println("NEW GAME");
+        awaitingConnections();
+    }
+
+    private class ClientConnection implements Runnable {
+
+        private BufferedReader in;
+        private PrintWriter out;
+        private Socket playerSocket;
+        private final String name;
+
+        private ClientConnection(Socket playerSocket, String name) {
+
+            this.playerSocket = playerSocket;
+            this.name = name;
+
+            try {
+
+                in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+                out = new PrintWriter(new OutputStreamWriter(playerSocket.getOutputStream()));
+
+            } catch (IOException e) {
+                System.out.println("Unable to establish a connection to client.");
+                System.exit(0);
+            }
+        }
+
+        public void close() {
+            try {
+                playerSocket.close();
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            String guess;
+            try {
+                while ((guess = in.readLine()) != null) {
+
+                    sendAll(guess);
+
+                    if (game.check(guess)) {
+                        sendAll("Detective " + name + " WINS THE GAME\n");
+                        win();
+                    } else {
+                        sendAll("Detective " + name + " failed! Next!\n");
+                    }
+
+                    next();
+                }
+
+                close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        private void send(String message) {
+            out.println(message);
+            out.flush();
+        }
+    }
+}
